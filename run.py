d5dcac14ab894f0b1329149300c19b100ac0166f
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-08-04 11:32:02 +0000

    Install and import colorama, add function for the app introduction

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+"""
+Import section
+"""
+import random
 import gspread
 from google.oauth2.service_account import Credentials
+from colorama import init
+init()
+from colorama import Fore
+
+"""
+Scope borrowed from Code Institute
+From Love Sandwiche's Walkthru Project
+"""
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +24,44 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('click_pose')
 
+
+"""
+Variables created to check proper connection with worksheets
+"""
 stretch = SHEET.worksheet('stretch')
+data_stretch = stretch.get_all_values()
+# print(data_stretch)
 
-data = stretch.get_all_values()
+strength = SHEET.worksheet('strength')
+data_strength = strength.get_all_values()
+# print(data_strength)
 
-print(data)
+torsion = SHEET.worksheet('torsion')
+data_torsion = torsion.get_all_values()
+# print(data_torsion)
+
+balance = SHEET.worksheet('balance')
+data_balance = balance.get_all_values()
+# print(data_balance)
+
+feedback = SHEET.worksheet('feedback')
+data_feedback = feedback.get_all_values()
+# print(data_feedback)
+
+
+def intro_click_pose():
+    """
+    Introduction of the application
+    with greeting, instructions and purpose.
+    """
+    print(Fore.MAGENTA + 'Wellcome to Click Pose!\n')
+    print(Fore.MAGENTA + 'Let the benefits of Yoga')
+    print(Fore.MAGENTA + 'indulge your body, mind and soul!')
+    print(Fore.BLUE + '\nEnter 1 to practice a STRETCH pose')
+    print(Fore.BLUE + 'Enter 2 to practice a STRENGTH pose')
+    print(Fore.BLUE + 'Enter 3 to practice a TORSION pose')
+    print(Fore.BLUE + 'Enter 4 to practice a BALANCE pose\n')
+    print(Fore.GREEN + "Don't forget to leave your feedback")
+    print(Fore.GREEN + "when finished" + "\U0001F64F\n")
+    
+intro_click_pose()
